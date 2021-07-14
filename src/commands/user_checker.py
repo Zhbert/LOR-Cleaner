7c80199744baa1aac07ae8187fb1cec3829d6622
Zhbert <zhbert@yandex.ru>
2021-07-14 12:32:27 +0300

    Some new code

--- src/commands/user_checker.py
+++ src/commands/user_checker.py
@@ -11,7 +11,7 @@ def check_user_status():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument("--remote-debugging-port=9222")
-    browser = webdriver.Chrome(options=chrome_options, executable_path="/Users/zhbert/chromedriver")
+    browser = webdriver.Chrome(options=chrome_options, executable_path="/home/zhbert/chromedriver")
     browser.get("https://linux.org.ru")
     user_div = browser.find_element_by_id('loginGreating')
     if user_div.text == "Регистрация - Вход":
--- src/services/settngs_file_service.py
+++ src/services/settngs_file_service.py
@@ -2,10 +2,38 @@
 #  Created by Zhbert.
 #  Licensed by GPLv3.
 import os
+import configparser
 
 
 def check_settings_file():
+    settings_path = get_home_path() + "/.lor_cleaner"
+    if os.path.exists(settings_path):
+        print("Найден каталог настроек. Читаю параметры логина...")
+    else:
+        print("Каталог настроек не найден: " + settings_path)
+        set_user_data()
+
+
+def get_home_path():
     home_path = (os.getenv('HOME'))
     if home_path == "None":
         home_path = os.getenv('USERPROFILE')
-    print(home_path)
+    return home_path
+
+
+def set_user_data():
+    settings_path = get_home_path() + "/.lor_cleaner"
+    config = configparser.ConfigParser()
+    config.add_section("Settings")
+    config.set("Settings", "font", "Courier")
+    config.set("Settings", "font_size", "10")
+    config.set("Settings", "font_style", "Normal")
+    config.set("Settings", "font_info",
+               "You are using %(font)s at %(font_size)s pt")
+    if os.path.exists(settings_path + "/lorcleaner.conf"):
+        with open(settings_path + "/lorcleaner.conf", "w") as config_file:
+            config.write(config_file)
+    else:
+        os.mkdir(settings_path)
+        with open(settings_path + "/lorcleaner.conf", "x") as config_file:
+            config.write(config_file)
